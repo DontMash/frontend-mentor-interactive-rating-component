06a67e6e061387a1166fbc51bd9cef47f4c9b659
Sören Maschmann <smaschmann@slashwhy.de>
2022-11-07 10:51:55 +0100

    Initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,svelte}'
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'orange': '#fb7413',
+      'white': '#ffffff',
+      'light-gray': '#959eac',
+      'medium-gray': '#7c8798',
+      'dark-blue': '	#252d37',
+      'very-dark-blue': '#121417',
+    },
+    fontFamily: {
+      'overpass': ['Overpass', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
